5f467563f48a1580868626ca851fa2e767c14612
gaozuqiang <68024818+gaozuqiang@users.noreply.github.com>
2021-10-21 21:03:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+/*document.querySelectorAll(".question").forEach((question)=>question.addEventListener("click",()=> {
+
+  if(question.parentNode.classList.contains("active")){
+    question.parentNode.classList.toggle("active")
+  }
+  else{
+    document.querySelectorAll(".question").forEach(question => question.parentNode.classList.remove("active"))
+
+    question.parentNode.classList.add("active")
+  }
+}))*/
+
+document.querySelectorAll(".question").forEach((question)=>question.addEventListener("click", ()=> {
+  console.log("hello")
+  console.log(question.parentNode.classList.contains("active"))
+  if(question.parentNode.classList.contains("active")){
+    question.parentNode.classList.toggle("active")
+  }
+  else{
+    document.querySelectorAll(".question").forEach(question=>question.parentNode.classList.remove("active"))
+
+    question.parentNode.classList.add("active")
+  }
+}))
